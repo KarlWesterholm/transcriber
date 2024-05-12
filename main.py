aee74930a2f10170e18c833d9e3ebc07a5cb3f9e
Karl <westerholm.karl@gmail.com>
2024-05-12 15:49:34 +0200

    Implemented retry logic into tiktok_video_details
    Now retries max 3 times when encountering error
    when obtaining the tiktok json and video details.
    Added new function for appending to existing df
    in main.py.
    
    Added progress bar and better stat read out.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import argparse
+import os
+import time
 
 import pandas as pd
+import numpy as np
 
-from tiktok_video_details import TiktokVideoDetails, VideoIsPrivateError, RequestReturnedNoneError
+from tiktok_video_details import (
+    TiktokVideoDetails,
+    VideoIsPrivateError,
+    RequestReturnedNoneError,
+    HTTPRequestError
+)
+
+def print_progress_bar(percentage: float, bar_length: int = 20) -> None:
+    normalizer = int(100 / bar_length)
+    progress = '\r[%s%s] %.2f%%' % (
+        '='*int(percentage/normalizer),
+        ' '*int(bar_length - percentage/normalizer),
+        percentage
+    )
+    print(progress, end='', flush=True)
 
 def get_tiktok_info(url: str = None, csv_filename: str = None):
     if url is None and csv_filename is None:
@@ -31,8 +47,8 @@ def get_tiktok_info(url: str = None, csv_filename: str = None):
                 print(error)
                 continue
 
-            tt_obj.save_data_to_csv_file("transcriptions.csv")
-            successes += 1
+            # tt_obj.save_data_to_csv_file("transcriptions.csv")
+            successes += tt_obj.save_data_to_csv_file("transcriptions.csv")
     except KeyboardInterrupt:
         print("Keyboard Interrupt detected. Stopping...")
     finally:
@@ -42,7 +58,87 @@ def get_tiktok_info(url: str = None, csv_filename: str = None):
         print("Private: ", len(private_videos))
         print("Failed: ", len(failed_requests))
 
+def save_tiktok_info_to_existing_csv(csv_filename: str):
+
+    df = pd.read_csv(csv_filename)
+
+    successes = 0
+    private_videos = []
+    failed_requests = []
+    total_rows = len(df)
+    errors = {}
+    en_transcriptions = {}
+    de_transcriptions = {}
+
+    start_time = time.time()
+
+    try:
+        for index, row in df.iterrows():
+            completion_percentage = (index/total_rows)*100
+            print_progress_bar(completion_percentage)
+            # url = f"https://www.tiktok.com/@{row["author_username"]}/video/{row["video_id"]}"
+            url = row["url"]
+            try:
+                tt_obj = TiktokVideoDetails(url=url)
+            except VideoIsPrivateError as error:
+                private_videos.append(url)
+                print('\n', error)
+                errors[index] = error
+                continue
+            except (RequestReturnedNoneError, HTTPRequestError) as error:
+                failed_requests.append(url)
+                print('\n', error)
+                errors[index] = error
+                continue
+            except Exception as error:
+                failed_requests.append(url)
+                print('\nUnexpected Exception occured:', error)
+                errors[index] = error
+                continue
+
+
+            try:
+                transcriptions = tt_obj.get_transcriptions(disable_azure=True)
+                if transcriptions:
+                    successes += 1
+                else:
+                    errors[index] = "No transcription provided by Tiktok"
+            except Exception as error:
+                print('\n', error)
+                transcriptions = {}
+                errors[index] = error
+
+            en_transcriptions[index] = transcriptions.get("eng-US", np.nan)
+            de_transcriptions[index] = transcriptions.get("deu-DE", np.nan)
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt detected. Stopping...")
+    except Exception as error:
+        print("\nUnexpected Exception occurred:", error)
+    finally:
+        end_time = time.time()
+        target_filename = os.path.splitext(csv_filename)[0] + '_transcribed.csv'
+        target_filename = 'test2.csv'
+        new_df = df.assign(
+            english_transcript=en_transcriptions,
+            german_transcript=de_transcriptions,
+            error_reason=errors)
+        new_df.to_csv(target_filename)
+
+        print("Private: \n", "\n".join(private_videos))
+        print("Failed: \n", "\n".join(failed_requests))
+        print("Successes: ", successes)
+        print("Private: ", len(private_videos))
+        print("Failed: ", len(failed_requests))
+        total_time = end_time - start_time
+        hours = total_time // 3600
+        minutes = (total_time % 3600) // 60
+        seconds = total_time - 3600 * hours - 60 * minutes
+        print("Total elapsed time: %dh %dm %.2fs" % (
+            hours,
+            minutes,
+            seconds
+        ))
 
 
 if __name__ == '__main__':
-    get_tiktok_info(csv_filename="./data/all_data_01-01-2024_06-05-2024.csv")
+    save_tiktok_info_to_existing_csv(csv_filename="./data/tiktok_videos_based_on_hashtags_cleaned.csv")
--- tiktok_video_details.py
+++ tiktok_video_details.py
@@ -1,6 +1,7 @@
 import os
 import io
 import requests
+from requests.exceptions import ReadTimeout, SSLError
 import re
 
 import pandas as pd
@@ -19,19 +20,39 @@ class VideoIsPrivateError(Exception):
 class RequestReturnedNoneError(Exception):
     pass
 
+class HTTPRequestError(Exception):
+    pass
 class TiktokVideoDetails:
     def __init__(self, url: str):
         self.transcription_source : str
         self.url = url
-        tt_json = pyk.alt_get_tiktok_json(self.url)
-        if tt_json is None:
-            print("ttjson is none")
-            raise RequestReturnedNoneError("Json request returned None. Please try again later.")
-        try:
-            self.details: dict = tt_json["__DEFAULT_SCOPE__"]["webapp.video-detail"]["itemInfo"]["itemStruct"]
-        except KeyError:
-            print("Keyerror")
-            raise VideoIsPrivateError("Video details could not be parsed. Video is private or has been removed.")
+        retries = 3
+        while retries > 0:
+            try:
+                tt_json = pyk.alt_get_tiktok_json(self.url)
+            except (ReadTimeout, SSLError):
+                retries -= 1
+                if retries == 0:
+                    raise HTTPRequestError("\nEncountered an error when making the http request.")
+                continue
+            except Exception:
+                retries -= 1
+                if retries == 0:
+                    raise
+                continue
+            if tt_json is None:
+                retries -= 1
+                if retries == 0:
+                    raise RequestReturnedNoneError("\nJson request returned None. Please try again later.")
+                continue
+            try:
+                self.details: dict = tt_json["__DEFAULT_SCOPE__"]["webapp.video-detail"]["itemInfo"]["itemStruct"]
+            except KeyError:
+                retries -= 1
+                if retries == 0:
+                    raise VideoIsPrivateError("\nVideo details could not be parsed. Video is private or has been removed.")
+                continue
+            break
 
     @property
     def description(self) -> str:
@@ -47,11 +68,12 @@ class TiktokVideoDetails:
 
     @property
     def download_url(self) -> str:
-        return self.details['video']['downloadAddr']
+        return self.details['video'].get('downloadAddr')
 
     @property
-    def duration(self) -> float:
-        return float(self.details["video"]["duration"])
+    def duration(self) -> int:
+        """Duration of the tiktok in seconds. Defaults to 20."""
+        return int(self.details["video"].get("duration"), 20)
 
 
     def get_transcriptions(self, disable_azure: bool = False) -> dict:
@@ -59,7 +81,7 @@ class TiktokVideoDetails:
         headers = {"User-Agent": user_agent}
         transcriptions = {}
 
-        for info in self.details["video"]["subtitleInfos"]:
+        for info in self.details["video"].get("subtitleInfos", []):
             if (language := info["LanguageCodeName"]) in ["eng-US", "deu-DE"] and info["Format"] == "webvtt":
                 result = requests.get(info["Url"], headers=headers)
                 if vtt := result.content.decode():
@@ -74,17 +96,17 @@ class TiktokVideoDetails:
                         continue
                     transcriptions[language] = transcript
 
-        if transcriptions:
-            self.transcription_source = "Tiktok"
-        elif not disable_azure:
+        self.transcription_source = "Tiktok"
+
+        if not transcriptions and not disable_azure:
             if self.has_original_sound:
                 transcriptions = self.get_transcription_from_azure()
                 self.transcription_source = "Azure Speech to Text"
 
-            # if not transcriptions:
-            #     video_filename = re.findall(pyk.url_regex, self.url)[0].replace("/", '_') + '.mp4' # taken from pyktok
-            #     transcriptions = AzureConnector.get_ocr_from_azure(self.download_url)
-            #     self.transcription_source = "Azure Video Indexer"
+            if not transcriptions:
+                video_filename = re.findall(pyk.url_regex, self.url)[0].replace("/", '_') + '.mp4' # taken from pyktok
+                # transcriptions = AzureConnector.get_ocr_from_azure(self.download_url)
+                self.transcription_source = "Azure Video Indexer"
 
 
         return transcriptions
@@ -93,7 +115,7 @@ class TiktokVideoDetails:
         # Gather video meta data
         meta_data = pyk.generate_data_row(video_obj=self.details)
 
-        transcriptions = self.get_transcriptions()
+        transcriptions = self.get_transcriptions(disable_azure=True)
 
         # Add custom desired info
         meta_data["suggested_words"] = " / ".join(self.suggested_words)
